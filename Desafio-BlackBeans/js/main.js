49033cd9982382f3d187a5bb2b36b4a5942650a8
KaueRonald <Kaueronald21.kr@gmail.com>
2024-06-06 16:41:49 -0300

    feat: adicionando imagens e arquivo js não utilizado

--- Desafio-BlackBeans/js/main.js
+++ Desafio-BlackBeans/js/main.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hello World');
+    console.log('Desafio Black Beans');
 });
+
+
+// Não achei aplicações reais para usar JavaScript, 
+// a não ser criando um carrousel do 0, mas não foi disponibilizado imagens também
+// e a imagem que foi, tinha nela os próprios dots de carrousel, então fiz estático
